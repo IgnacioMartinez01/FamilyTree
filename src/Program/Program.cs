0b7b4a8cc21cee7f791e3f2b20d3f9d49e8418f6
IgnacioMartinez01 <igmartinezpintosgmail.com>
2023-06-08 16:04:17 -0300

    Trabajo hecho

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,13 +7,27 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Node n1 = new Node(1);
-            Node n2 = new Node(2);
-            Node n3 = new Node(3);
-            Node n4 = new Node(4);
-            Node n5 = new Node(5);
-            Node n6 = new Node(6);
-            Node n7 = new Node(7);
+            
+
+           
+
+            Person p1 = new Person("Ignacio",20);
+            Person p2 = new Person ("Tomas",19);
+            Person p3 = new Person("Bruno",20);
+            Person p4 = new Person("Marcelo",20);
+            Person p5 = new Person("Jorge",20);
+            Person p6 = new Person("Gaston",20);
+            Person p7 = new Person("Juan",20);
+            Person p8 = new Person("Ricardo",20);
+
+            Node n1 = new Node(p1);
+            Node n2 = new Node(p2);
+            Node n3 = new Node(p3);
+            Node n4 = new Node(p4);
+            Node n5 = new Node(p5);
+            Node n6 = new Node(p6);
+            Node n7 = new Node(p7);
+            Node n8 = new Node(p8);
 
             n1.AddChildren(n2);
             n1.AddChildren(n3);
@@ -21,9 +35,16 @@ namespace Program
             n2.AddChildren(n4);
             n2.AddChildren(n5);
 
-            n3.AddChildren(n6);
+            n2.AddChildren(n6);
             n3.AddChildren(n7);
+            n3.AddChildren(n8);
 
+            TotalEdad total=new TotalEdad();
+            total.Visitado(n1);
+            MayorHijo mayor=new MayorHijo();
+            mayor.Visitado(n1);
+            Console.Write(total.Total);
+            Console.Write(mayor.Mayor);
             // visitar el árbol aquí
         }
     }
